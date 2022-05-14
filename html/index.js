d65d82b827bcce8994e6dec69017f8c7578f0cd7
kako-jun <3541096+kako-jun@users.noreply.github.com>
2022-05-14 10:01:54 +0900

    update.

---
+++ html/index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// const urlRoot = "http://localhost:20221/api";
+const urlRoot = "https://chacha.llll-ll.com/api";
+
+const sendClicked = () => {
+  const textarea = document.querySelector("#serif");
+  if (textarea.value !== "") {
+    sendSerif({ text: textarea.value });
+    textarea.value = "";
+  }
+};
+
+const sendSerif = async (serif) => {
+  console.log(serif);
+
+  const url = `${urlRoot}/serifs`;
+  const res = await fetch(url, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(serif),
+  });
+
+  console.log(res);
+  return res.json();
+};
